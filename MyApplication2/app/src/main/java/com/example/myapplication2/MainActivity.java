f994dc527830b850b2f18aa62cb795911a9e7f64
chthao123 <63527673+chthao123@users.noreply.github.com>
2020-10-06 15:18:23 -0700

    update main-activity
    The previous merge change the code

--- MyApplication2/app/src/main/java/com/example/myapplication2/MainActivity.java
+++ MyApplication2/app/src/main/java/com/example/myapplication2/MainActivity.java
@@ -2,10 +2,9 @@ package com.example.myapplication2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-<<<<<<< Updated upstream
-=======
+
 import android.content.Intent;
->>>>>>> Stashed changes
+
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
@@ -17,51 +16,34 @@ public class MainActivity extends AppCompatActivity {
     EditText username, password;
     Button btnLOGIN;
     Button btn2REGISTER;
-<<<<<<< Updated upstream
+
 
     String correct_username = "customer";
     String correct_password = "mycars1234";
-=======
+
     DBHelper DB;
 
     //String correct_username = "customer";
     //String correct_password = "mycars1234";
->>>>>>> Stashed changes
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-<<<<<<< Updated upstream
-=======
 
->>>>>>> Stashed changes
+
         username = findViewById(R.id.username);
         password = findViewById(R.id.password);
         btnLOGIN = findViewById(R.id.button);
         btn2REGISTER = findViewById(R.id.button2);
-<<<<<<< Updated upstream
-=======
+
         DB = new DBHelper(this);
->>>>>>> Stashed changes
+
 
         btnLOGIN.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-<<<<<<< Updated upstream
-                if (TextUtils.isEmpty(username.getText().toString()) || TextUtils.isEmpty(password.getText().toString())) {
-                    Toast.makeText(MainActivity.this, "Empty", Toast.LENGTH_LONG).show();
-                } else if (username.getText().toString().equals(correct_username)) {
-                    if ((password.getText().toString().equals(correct_password))) {
-
-                        Toast.makeText(MainActivity.this, "Successful Username/Password", Toast.LENGTH_LONG).show();
-                    }
-                } else {
-                    Toast.makeText(MainActivity.this, "Invalid Username/Password", Toast.LENGTH_LONG).show();
-                }
-            }
-        });
-=======
+                    @Override
+                    public void onClick(View v) {
                 String user = username.getText().toString();
                 String pass = password.getText().toString();
 
@@ -88,6 +70,5 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
->>>>>>> Stashed changes
     }
 }
