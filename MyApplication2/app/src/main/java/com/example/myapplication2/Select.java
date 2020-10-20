21eb95dd00347ed4a506d386a8c8067b3b045c48
ajpongress <ajpongress@gmail.com>
2020-10-19 17:25:16 -0700

    Create Select.java

---
+++ MyApplication2/app/src/main/java/com/example/myapplication2/Select.java
@@ -0,0 +1,16 @@
+package com.example.myapplication2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Select extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select);
+    }
+}
