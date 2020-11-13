e7ff7d7ca6bdb3c556a3c341e255f8edc003db01
ajpongress <ajpongress@gmail.com>
2020-11-13 12:53:02 -0800

    Create Calendar.java

---
+++ MyApplication2/app/src/main/java/com/example/myapplication2/Calendar.java
@@ -0,0 +1,17 @@
+package com.example.myapplication2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Calendar extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calendar);
+
+
+
+    }
+}
